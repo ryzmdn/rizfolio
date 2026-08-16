50b9c5838b20610c33845022c0b29f684ef1cfb7
ryzmdn <ryzmdn@gmail.com>
2026-08-17 04:31:54 +0700

    feat: initialize new Next.js application in shop directory

---
+++ apps/shop/next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  transpilePackages: ["@workspace/ui"],
+  reactStrictMode: true,
+  poweredByHeader: false,
+  compress: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+}
+
+export default nextConfig
