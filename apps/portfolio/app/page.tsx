b0f1b90fbf9c605824fdfa9249b3d942e5efd207
ryzmdn <ryzmdn@gmail.com>
2026-08-16 07:33:04 +0700

    feat: implement portfolio homepage with biography, engineering process, and services sections

--- apps/portfolio/app/page.tsx
+++ apps/portfolio/app/page.tsx
@@ -399,6 +399,45 @@ export default function Home() {
 
       <Container className="py-20">
         <hgroup className="w-full space-y-2">
+          <h2 className="text-2xl font-medium">Edducation</h2>
+
+          <div className="leading-7 text-muted-foreground">
+            <p>
+              Helping businesses build intuitive products that drive growth.
+            </p>
+          </div>
+        </hgroup>
+
+        <div className="w-full bg-transparent py-12">
+          {[1, 2].map((_, idx) => (
+            <div
+              key={_}
+              className="grid w-full items-center pb-12 lg:grid-cols-4"
+            >
+              <div className="w-full text-sm/6">
+                <h3 className="relative w-max text-2xl font-medium text-primary">
+                  ABCD University{" "}
+                  <small className="absolute -right-5 text-xs opacity-50">
+                    0{idx + 1}
+                  </small>
+                </h3>
+              </div>
+              <div className="text-center text-muted-foreground lg:col-span-2">
+                <p>Bachelor of Engineering</p>
+                <p>BE, Computer Science</p>
+              </div>
+              <div className="flex items-center justify-end gap-x-2 leading-7 text-muted-foreground">
+                <p>2023</p>
+                <span>&ndash;</span>
+                <p>2027</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Container>
+
+      <Container className="py-20">
+        <hgroup className="w-full space-y-2">
           <h2 className="text-2xl font-medium">Experience</h2>
 
           <div className="leading-7 text-muted-foreground">
